961479b0e2985ecaf7cd3749e92574570df9f051
kpako3rbp <hansss@list.ru>
2024-03-10 03:06:54 +0400

    add 404 page

--- pages/404.js
+++ pages/404.js
@@ -2,10 +2,18 @@ import React from 'react';
 
 import { fetchPostsPaths, loadCurrentPost } from '@/pages/api/currentPost';
 import { loadPosts } from '@/pages/api/posts';
+import Section from '@/shared/ui/Section';
+import NotFoundBlock from "@/shared/ui/NotFoundBlock";
 import Layout from '@/shared/ui/Layout';
 
 const NotFound = () => {
-  return <Layout>Страница не найдена</Layout>;
+  return (
+    <Layout>
+      <Section>
+        <NotFoundBlock />
+      </Section>
+    </Layout>
+  );
 };
 
 export default NotFound;
